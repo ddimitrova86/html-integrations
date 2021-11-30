829af6a60736b462981a469b505e3af7e7a1e431
Diyana Dimitrova <diyana.dimitrova@primeholding.com>
2021-11-30 14:56:26 +0200

    add custom toolbar

--- demos/angular/generic/src/app/app.component.ts
+++ demos/angular/generic/src/app/app.component.ts
@@ -26,9 +26,14 @@ export class AppComponent implements OnInit {
     // Load the toolbar and the editable area into const variables to work easy with them
     const editableDiv = document.getElementById('htmlEditor');
     const toolbarDiv = document.getElementById('toolbar');
+    // custom toolbar
+    const toolbar = '<toolbar removeLinks="true"><tab name=""><section rows="1"><createButton icon="https://i.ibb.co/Jjc3xk1/sum.png" title="Plus"><content><mo mathvariant="bold" mathcolor="#27ae63">+</mo></content></createButton><createButton icon="https://i.ibb.co/Gd436RP/rest.png" title="Minus"><content><mo mathvariant="bold" mathcolor="#16a086">-</mo></content></createButton><createButton icon="https://i.ibb.co/Ttxqhfw/mult.png" title="Multiplication"><content><mo mathvariant="bold" mathcolor="#953ead">&#xD7;</mo></content></createButton><createButton icon="https://i.ibb.co/D57RV4B/division.png" title="Division"><content><mo mathvariant="bold" mathcolor="#c53221">&#xF7;</mo></content></createButton><createButton icon="https://i.ibb.co/C0n1MLT/equal.png" title="Equal"><content><mo mathvariant="bold" mathcolor="#d15504">=</mo></content></createButton><createButton icon="https://i.ibb.co/hHNDbPC/notEqual.png" title="Not equal"><content><mo mathvariant="bold" mathcolor="#f89d0d">&#x2260;</mo></content></createButton><createButton icon="https://i.ibb.co/9GkLxdS/lessThan.png" title="Less than"><content><mo mathvariant="bold" mathcolor="#5d6564">&#x2264;</mo></content></createButton><createButton icon="https://i.ibb.co/d4njMcB/greater-Than.png" title="Greater than"><content><mo mathvariant="bold" mathcolor="#818c8b">&#x2265;</mo></content></createButton><createButton icon="https://i.ibb.co/ydwqnb6/percent.png" title="Percent"><content><mo mathvariant="bold" mathcolor="#ff6440">%</mo></content></createButton></section></tab></toolbar>';
+    const integrationParameters = {
+      editorParameters : { 'toolbar' : toolbar }
+    }
 
     // Initialyze the editor.
-    wrsInitEditor(editableDiv, toolbarDiv);
+    wrsInitEditor(editableDiv, toolbarDiv, integrationParameters);
 
     // Add listener on click button to launch updateContent function.
     document.getElementById('btn_update').addEventListener('click', (e) => {
